0df7a46de83c9098c562fdbcf9423967147e8349
khalid4747 <kawad4747@gmail.com>
2020-10-01 22:20:25 -0400

    Working on data model branch

---
+++ QuorumDB/Models/ModModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quorum.Model
+{
+    public class ModModel
+    {
+        public int ModId { get; set; }
+        public string UserId { get; set; }
+        public int QuorumId { get; set; }
+    }
+}
