d9b4519c6ba0d660f3921f614cc866d38cb163eb
Wameed Miriako <wameed.miriako@wayne.edu>
2020-11-12 23:25:01 -0500

    Adapter Design Pattern

--- Quorum/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ Quorum/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -12,8 +12,7 @@ using QuorumDB.Models;
 
 namespace Quorum.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class ConfirmEmailChangeModel : PageModel
+    interface ConfirmEmail
     {
         private readonly UserManager<AspNetUser> _userManager;
         private readonly SignInManager<AspNetUser> _signInManager;
@@ -26,10 +25,14 @@ namespace Quorum.Areas.Identity.Pages.Account
 
         [TempData]
         public string StatusMessage { get; set; }
+        public void check();
+    }
 
-        public async Task<IActionResult> OnGetAsync(string userId, string email, string code)
+    class UserInsert : ConfirmEmail
+    {
+        public void check(string userId, string code)
         {
-            if (userId == null || email == null || code == null)
+            if (userId == null)
             {
                 return RedirectToPage("/Index");
             }
@@ -39,6 +42,16 @@ namespace Quorum.Areas.Identity.Pages.Account
             {
                 return NotFound($"Unable to load user with ID '{userId}'.");
             }
+        }
+    }
+    class EmailInsert : ConfirmEmail
+    {
+        public void check(string email, string code)
+        {
+            if (email == null)
+            {
+                return RedirectToPage("/Index");
+            }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ChangeEmailAsync(user, email, code);
@@ -47,19 +60,8 @@ namespace Quorum.Areas.Identity.Pages.Account
                 StatusMessage = "Error changing email.";
                 return Page();
             }
-
-            // In our UI email and user name are one and the same, so when we update the email
-            // we need to update the user name.
-            var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
-            if (!setUserNameResult.Succeeded)
-            {
-                StatusMessage = "Error changing user name.";
-                return Page();
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Thank you for confirming your email change.";
-            return Page();
         }
     }
+
+
 }
