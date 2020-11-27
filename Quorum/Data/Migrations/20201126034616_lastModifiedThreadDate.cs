8b9bfb3e5124011637e13bc0e3c97890b3116fcd
khalid4747 <kawad4747@gmail.com>
2020-11-27 02:16:18 -0500

    Forumpage fixes

---
+++ Quorum/Data/Migrations/20201126034616_lastModifiedThreadDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quorum.Migrations
+{
+    public partial class lastModifiedThreadDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastActivity",
+                table: "ForumThreads",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastActivity",
+                table: "ForumThreads");
+        }
+    }
+}
