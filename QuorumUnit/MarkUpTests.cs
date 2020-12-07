16e560a676566ff1489cd450fb504bba315b9335
khalid4747 <kawad4747@gmail.com>
2020-12-07 16:41:54 -0500

    Make images responsive

--- QuorumUnit/MarkUpTests.cs
+++ QuorumUnit/MarkUpTests.cs
@@ -15,7 +15,7 @@ namespace QuorumUnit
     /// These tests are written entirely in C#.
     /// Learn more at https://bunit.egilhansen.com/docs/
     /// </summary>
-    public class CounterCSharpTests : TestContext
+    public class MarkUpTests : TestContext
     {
         [Fact]
         public void MarkupDirtyInput()
@@ -64,13 +64,25 @@ namespace QuorumUnit
         [Fact]
         public void MarkupMultiMediaInput()
         {
-            string input = @"![bug](https://64.media.tumblr.com/a173ece72bab1e6648c99ad4162eb046/tumblr_pw9hldCvv31us4qppo3_r1_500.gif){width=128 height=128}";
+            string input = @"![bug](https://64.media.tumblr.com/a173ece72bab1e6648c99ad4162eb046/tumblr_pw9hldCvv31us4qppo3_r1_500.gif)";
 
             //Can add a service regularly 
             Services.AddSingleton<IHtmlSanitizer, HtmlSanitizer>();
 
             var cut = RenderComponent<Markdown>(Parameter("InputText", input));
-            cut.Find("p").MarkupMatches(@"<p><img src=""https://64.media.tumblr.com/a173ece72bab1e6648c99ad4162eb046/tumblr_pw9hldCvv31us4qppo3_r1_500.gif"" width=""128"" height=""128"" alt=""bug""></p>");
+            cut.Find("p").MarkupMatches(@"<p><img src=""https://64.media.tumblr.com/a173ece72bab1e6648c99ad4162eb046/tumblr_pw9hldCvv31us4qppo3_r1_500.gif"" class=""img-fluid"" alt=""bug""></p>");
+        }
+
+        [Fact]
+        public void MarkupMedia()
+        {
+            string input = @"![youtube.com](https://www.youtube.com/watch?v=mswPy5bt3TQ)";
+
+            //Can add a service regularly 
+            Services.AddSingleton<IHtmlSanitizer, HtmlSanitizer>();
+
+            var cut = RenderComponent<Markdown>(Parameter("InputText", input));
+            cut.Find("p").MarkupMatches("<p><iframe src=\"https://www.youtube.com/embed/mswPy5bt3TQ\" class=\"img-fluid youtube\" width=\"500\" height=\"281\" frameborder=\"0\" allowfullscreen=\"\"></iframe></p>");
         }
     }
 }
