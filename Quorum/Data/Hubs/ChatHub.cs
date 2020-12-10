ce3b7b6884498c03bc45fbc2c375309c94ad0361
khalid4747 <kawad4747@gmail.com>
2020-12-10 11:25:51 -0500

    Chat wip

---
+++ Quorum/Data/Hubs/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quorum.Data.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public const string HubUrl = "/chat";
+
+        public ChatHub(IConnectionManager manager)
+        {
+
+        }
+
+        public async Task SendMessage(string username, Circuit circuit)
+        {
+
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+    }
+}
