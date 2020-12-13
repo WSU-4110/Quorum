4ab87ab6108f4f09c2d3166da086fed8f07d0c97
fbuggia1 <71612806+fbuggia1@users.noreply.github.com>
2020-12-13 11:20:19 -0500

    Revert "Revert "Merge branch 'master' into Francis_Buggia_unittest""
    This reverts commit 9b98c316afbefd4c7a354c5e3c0022ed16c2e221.

---
+++ QuorumDB/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuorumDB.Models
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public string UserName { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Quorum
+# The Quorum website can be accessed here: <i>https://thequorum.azurewebsites.net/</i>
